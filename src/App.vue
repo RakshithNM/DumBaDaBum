015682ca8a79d38bcf83552e1415436147497a62
RakshithNM <rakshith_bellare@outlook.com>
2018-09-23 22:47:47 +0530

    Key press animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="keys">
-    <div v-for="key in keys" class="key">
+    <div v-for="key in keys" class="key" :class="{ 'playing': key.pressing }">
       <kbd>{{ key.keyName }}</kbd>
       <span class="sound">{{ key.text }}</span>
     </div>
@@ -12,15 +12,15 @@
     export default {
       data() {
         return {
-          keys: [{ keyCode: 65, keyName: 'A', text: "clap", src: "clap.wav" },
-                 { keyCode: 83, keyName: 'S', text: "hihat", src: "hihat.wav" },
-                 { keyCode: 68, keyName: 'D', text: "kick", src: "kick.wav" },
-                 { keyCode: 70, keyName: 'F', text: "openhat", src: "openhat.wav" },
-                 { keyCode: 71, keyName: 'G', text: "boom", src: "boom.wav" },
-                 { keyCode: 72, keyName: 'H', text: "ride", src: "ride.wav" },
-                 { keyCode: 74, keyName: 'J', text: "snare", src: "snare.wav" },
-                 { keyCode: 75, keyName: 'K', text: "tom", src: "tom.wav" },
-                 { keyCode: 76, keyName: 'L', text: "tink", src: "tink.wav" }]
+          keys: [{ keyCode: 65, keyName: 'A', text: "clap", src: "clap.wav", pressing: false },
+                 { keyCode: 83, keyName: 'S', text: "hihat", src: "hihat.wav", pressing: false },
+                 { keyCode: 68, keyName: 'D', text: "kick", src: "kick.wav", pressing: false },
+                 { keyCode: 70, keyName: 'F', text: "openhat", src: "openhat.wav", pressing: false },
+                 { keyCode: 71, keyName: 'G', text: "boom", src: "boom.wav", pressing: false },
+                 { keyCode: 72, keyName: 'H', text: "ride", src: "ride.wav", pressing: false },
+                 { keyCode: 74, keyName: 'J', text: "snare", src: "snare.wav", pressing: false },
+                 { keyCode: 75, keyName: 'K', text: "tom", src: "tom.wav", pressing: false },
+                 { keyCode: 76, keyName: 'L', text: "tink", src: "tink.wav", pressing: false }]
         }
       },
       methods: {
@@ -32,6 +32,10 @@
             else {
               const audio = new Audio(`./src/assets/${key.src}`);
               audio.play();
+              key.pressing = true;
+              setTimeout(() => {
+                key.pressing = false;
+              }, 300);
             }
           });
         }
